34f2430d333bc667155f2ec57dd43cf4774be974
DavidFranco3 <josedavidayalafranco3@gmail.com>
2023-03-24 10:27:27 -0600

    cambio de api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
 // export const API_HOST = "http://localhost:5050";
 
 // Constantes para puesta en produccion
-export const API_HOST = "https://la-nena-api.vercel.app";
+export const API_HOST = "la-nena-api-antiguo.vercel.app";
 
 export const API_CLOUDINARY = "https://api.cloudinary.com/v1_1/omarlestrella/image/upload";
 
